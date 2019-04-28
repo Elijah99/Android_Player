b167cc27c0de8550d6cb50aa2e47c165ea967d3a
elijah <xronos928@gmail.com>
2019-04-28 17:36:03 +0300

    Still try to debug my code
    Signed-off-by: elijah <xronos928@gmail.com>

--- app/src/main/java/com/example/android_player/StorageUtil.java
+++ app/src/main/java/com/example/android_player/StorageUtil.java
@@ -14,7 +14,7 @@ import java.lang.reflect.Type;
  */
 public class StorageUtil {
 
-    private final String STORAGE = " com.valdioveliu.valdio.audioplayer.STORAGE";
+    private final String STORAGE = " com.example.android_player.STORAGE";
     private SharedPreferences preferences;
     private Context context;
 
